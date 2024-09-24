5cee2a9b57490e3ace1cd13e874b89ce229d3742
s-malan <24227013@sun.ac.za>
2024-09-24 16:08:36 +0200

    refactor

--- extract_seg.py
+++ extract_seg.py
@@ -6,20 +6,15 @@ Contact: 24227013@sun.ac.za
 Date: March 2024
 """
 
-from wordseg import utils, segment, evaluate
+from wordseg import utils, segment
 from tqdm import tqdm
 import numpy as np
 import argparse
 import json
 import os
 from pathlib import Path
-import matplotlib.pyplot as plt
-import torch
-import torchaudio
-from glob import glob
-import itertools
 
-def get_features(data, batch_num):
+def get_features(data, batch_size, batch_num):
     """
     Samples, loads and normalizes features of the audio data.
 
@@ -39,11 +34,11 @@ def get_features(data, batch_num):
     """
 
     sample = data.sample_features() # sample from the features
-    if 25000*(batch_num+1) <= len(sample):
-        sample = sample[25000*batch_num:25000*(batch_num+1)]
+    if batch_size*(batch_num+1) <= len(sample):
+        sample = sample[batch_size*batch_num:batch_size*(batch_num+1)]
         batch = True
     else:
-        sample = sample[25000*batch_num:]
+        sample = sample[batch_size*batch_num:]
         batch = False
     batch_num = batch_num + 1
 
@@ -118,82 +113,6 @@ def set_alignments(data, sample):
     data.set_alignments(files=alignments) # set the text, start and end attributes of the alignment files
 
 if __name__ == "__main__":
-    def plot_seg(segment, data, peaks, sample):
-        """
-        Plots the distances, moving average, hypothesized peaks and ground truth alignments for the first file in the sample 
-
-        Parameters
-        ----------
-        segment : Segmentor Class
-            Object containing all hyperparameters and methods to segment the features into words
-        data : Features Class
-            Object containing all information to find alignments for the selected features
-        peaks : list (int)
-            The frame indices of the detected peaks
-        """
-
-        # Plot Mel spectrogram:
-        f_s = 16000
-        n_fft = int(0.02*f_s) #20ms window length
-        stride = int(0.0201*f_s) #20ms to result in same number of frames as the speech models
-        transform = torchaudio.transforms.MelSpectrogram(sample_rate=f_s, n_fft=n_fft, hop_length=stride, n_mels=64)
-
-        sample = sample[0]
-        sample = os.path.split(sample)[-1]
-        file_path = glob(os.path.join("/media/hdd/data/librispeech/dev-clean", "**" , Path(sample).with_suffix(".flac")), recursive=True)[0]
-        wav, _ = torchaudio.load(file_path, backend='soundfile')
-        mel_specgram = np.log(transform(wav))
-        # mel_specgram = mel_specgram.permute(0, 2, 1)
-        if torch.isinf(mel_specgram).any():
-            mel_specgram[torch.isinf(mel_specgram)] = torch.nan
-
-        fig, ax = plt.subplots(figsize=(9, 5))
-        ax.imshow(mel_specgram.squeeze(), origin="lower", interpolation="nearest", zorder=0)
-
-        # Plot the distances, moving average and peaks
-        peaks = peaks[0]
-        distances = segment.distances[0]*13 + (13)
-        smoothed_distances = segment.smoothed_distances[0]*13 + (13)
-        
-        # _, ax = plt.subplots()
-        ax.plot(distances, label='Distances', color='red', alpha=0.7, linewidth=1.8, zorder=1)
-        # ax.plot(np.range(segment.distances[0]), segment.distances[0] label='Distances', color='blue')
-        ax.plot(smoothed_distances, label='Smooth Distances', color='white', alpha=1.0, linewidth=2.45, zorder=2)
-        ax.scatter(peaks, smoothed_distances[peaks], marker='+', label='Peaks', color='black', s=200, linewidth=2.45, zorder=3)
-        ax.scatter(peaks, smoothed_distances[peaks], marker='+', label='Peaks', color='white', s=150, linewidth=2.15, zorder=4)
-
-        ax.fill_between([-5, len(smoothed_distances)], [-0.5,-200], color='white')
-
-        alignment_end_frames = data.alignment_data[0].end
-        alignment_end_times = [data.get_sample_second(end_frame) for end_frame in alignment_end_frames]
-        segments = [(a + ((b-a)//2)) for a, b in itertools.pairwise(np.concatenate(([0],alignment_end_frames)))]
-        print('Segment end times, frames, and text')
-        print(alignment_end_times)
-        print(alignment_end_frames)
-        print(segments)
-        print(data.alignment_data[0].text)
-
-        ax.set_ylim(ymin=-10)
-        for frame in alignment_end_frames:
-            ax.axvline(x=frame, label='Ground Truth', color='black', linewidth=0.8)
-            # ax.axvline(x=frame-data.get_frame_num(0.02), color='black', linewidth=0.2, alpha=0.5) # 0.02s tolerance
-            # ax.axvline(x=frame+data.get_frame_num(0.02), color='black', linewidth=0.2, alpha=0.5)
-        
-        custom_tick_labels = data.alignment_data[0].text
-        ax.set_xticks([])
-        ax.set_yticks([])
-        font_size = 15/alignment_end_frames[-1] * 140
-        y_lim = 150/alignment_end_frames[-1] * - 6
-        # y_lim = (font_size * -0.8) - (font_size*0.025)
-        for i, midpoint in enumerate(segments):
-            ax.text(midpoint, -1, custom_tick_labels[i], ha='center', va='top', fontsize=font_size)
-
-        ax.axhline(y=0, color='black', linewidth=0.8)
-        ax.set_xlim(xmin=alignment_end_frames[0]-5)
-        ax.set_xlim(xmax=alignment_end_frames[-2]+5)
-        ax.set_ylim(ymin=y_lim, ymax=63)
-        plt.tight_layout()
-        plt.savefig('distances.pdf', bbox_inches='tight')
   
     parser = argparse.ArgumentParser(description="Segment speech audio.")
     parser.add_argument(
@@ -212,22 +131,10 @@ if __name__ == "__main__":
         type=Path,
     )
     parser.add_argument(
-        "alignments_dir",
-        metavar="alignments-dir",
-        help="path to the alignments directory.",
-        type=Path,
-    )
-    parser.add_argument(
         "sample_size",
         metavar="sample-size",
         help="number of features to sample (-1 to sample all available data).",
         type=int,
-    )
-    parser.add_argument(
-        "--align_format",
-        help="extension of the alignment files (defaults to .TextGrid).",
-        default=".TextGrid",
-        type=str,
     )
     parser.add_argument(
         "--save_out",
@@ -240,9 +147,10 @@ if __name__ == "__main__":
         default=None,
         type=Path,
     )
-    parser.add_argument( # optional argument to make the evaluation strict
-        '--strict',
-        action=argparse.BooleanOptionalAction
+    parser.add_argument(
+        '--batch_size',
+        default=25000,
+        int=Path,
     )
 
     args = parser.parse_args()
@@ -265,15 +173,15 @@ if __name__ == "__main__":
         frames_per_ms = 10
     else:
         frames_per_ms = 20
-    data = utils.Features(root_dir=args.features_dir, model_name=args.model, layer=args.layer, data_dir=args.alignments_dir, alignment_format=args.align_format, num_files=args.sample_size, frames_per_ms=frames_per_ms)
+    data = utils.Features(root_dir=args.features_dir, model_name=args.model, layer=args.layer, num_files=args.sample_size, frames_per_ms=frames_per_ms)
 
-    # features
+    # Features
     batch_num = 0
     batch = True
 
     while batch == True: # process in batches to avoid memory issues
         print("Batch number: ", batch_num)
-        sample, features, norm_features, index_one_frame, batch, batch_num = get_features(data, batch_num)
+        sample, features, norm_features, index_one_frame, batch, batch_num = get_features(data, args.batch_size, batch_num)
 
         # Remove features with only one frame
         sample_one_frame = [sample[i] for i in index_one_frame]
@@ -310,21 +218,5 @@ if __name__ == "__main__":
                 with open(save_dir, "w") as f: # save the landmarks to a file
                     for l in peak:
                         f.write(f"{l}\n")
-
-        # Alignments
-        align_sample = data.get_alignment_paths(sample)
-        data.set_alignments(align_sample)
-
-        # Evaluate
-        alignment_end_frames = [alignment.end for alignment in data.alignment_data[:]]
-
-        p, r, f = evaluate.eval_segmentation(peaks, alignment_end_frames, strict=args.strict)
-        rval = evaluate.get_rvalue(p, r)
-        print('Evaluation: \n Precision: ', p, '\n Recall: ', r, '\n F1-Score: ', f, '\n R-Value: ', rval)
-        
-        # Plot the distances, moving average and peaks AND compare to the alignments of a single file
-        plot_seg(segmentor, data, peaks, sample)
-
-        del align_sample, alignment_end_frames
 
         del sample, features, norm_features, peaks, prominences, segmentor # clear memory for next batch
--- wordseg/utils.py
+++ wordseg/utils.py
@@ -1,5 +1,5 @@
 """
-Utility functions to sample audio embeddings, normalize them, and get the corresponding alignments with their attributes.
+Utility functions to sample audio features, normalize them, and get the corresponding alignments with their attributes.
 
 Author: Simon Malan
 Contact: 24227013@sun.ac.za
@@ -11,181 +11,106 @@ import torch
 from glob import glob
 from tqdm import tqdm
 import os
-from pathlib import Path
-import textgrids # https://pypi.org/project/praat-textgrids/
-from sklearn.preprocessing import StandardScaler # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
-
-class Alignment_Data:
-    """
-    The object containing all information of a specific alignment file
-
-    Parameters
-    ----------
-    dir : String
-        The path to the alignment data
-    alignment_format : String
-        Format of the alignment file ('TextGrid', 'txt')
-    type : String
-        The type of alignment to extract from the file (e.g. 'words', 'phones')
-        default: 'words'
-
-    Attributes
-    ----------
-    text : list (String)
-        The text of the alignment
-    start : list (int)
-        The start frame of the alignment
-    end : list (int)
-        The end frame of the alignment
-    """
-    def __init__(
-        self, dir, alignment_format, type='words'
-    ):
-        self.dir = dir
-        self.alignment_format = alignment_format
-        self.type = type
-        self.text = []
-        self.layer = []
-        self.start = []
-        self.end = []
-
-    def set_attributes(self, features):
-        """
-        Sets the text, start and end attributes of the object from the alignment file
-
-        Parameters
-        ----------
-        self : Class
-            The object containing all information of a specific alignment file
-        """
-
-        if self.alignment_format == '.TextGrid':
-            for word in textgrids.TextGrid(self.dir)[self.type]:
-                self.text.append(word.text)
-                self.start.append(float(word.xmin))
-                self.end.append(float(word.xmax))
-        elif self.alignment_format == '.txt':
-            with open(self.dir, 'r') as f:
-                for line in f:
-                    line = line.split()
-                    self.start.append(float(line[0]))
-                    self.end.append(float(line[1]))
-                    self.text.append(line[2])
-        
-        self.start = features.get_frame_num(np.array(self.start)).astype(int)
-        self.end = features.get_frame_num(np.array(self.end)).astype(int)
-    
-    def __str__(self):
-        return f"Alignment_Data({self.dir}, {self.text}, {self.start}, {self.end})"
+from sklearn.preprocessing import StandardScaler
 
 class Features:
     """
-    The object containing all information to find alignments for the selected embeddings.
+    The object containing all information to find alignments for the selected features.
 
     Parameters
     ----------
     root_dir : String
-        The path to the root directory of the embeddings
+        The path to the root directory of the features
     model_name : String
-        The name of the model to get the embeddings from
+        The name of the model to get the features from
     layer : int
-        The number of the layer to get the embeddings from
-    data_dir : String
-        The path to the root directory of the alignments
-    alignment_format : String
-        Format of the alignment files ('.TextGrid', '.txt')
-        default: '.TextGrid'
+        The number of the layer to get the features from
     num_files : int
-        The number of embeddings (utterances) to sample
+        The number of features (utterances) to sample
         default: 2000
     frames_per_ms : int
         The number of frames a model processes per millisecond
         default: 20
-    alignment_data : list (Alignment_Data)
-        The objects containing all information of a specific alignment file
     """
 
     def __init__(
-        self, root_dir, model_name, layer, data_dir, alignment_format='.TextGrid', num_files=2000, frames_per_ms=20
+        self, root_dir, model_name, layer, num_files=2000, frames_per_ms=20
     ):
         self.root_dir = root_dir
         self.model_name = model_name
         self.layer = layer
-        self.data_dir = data_dir
-        self.alignment_format = alignment_format
         self.num_files = num_files
         self.frames_per_ms = frames_per_ms
-        self.alignment_data = []
 
-    def sample_features(self): # Works
+    def sample_features(self):
         """
-        Randomly samples embeddings from the specified model and returns the file paths as a list.
+        Randomly samples features from the specified model and returns the file paths as a list.
 
         Parameters
         ----------
         self : Class
-            The object containing all information to find alignments for the selected embeddings
+            The object containing all information to find alignments for the selected features
 
         Return
         ------
-        embeddings_sample : list
-            List of file paths to the sampled embeddings
+        features_sample : list
+            List of file paths to the sampled features
         """
 
         if self.layer != -1:
             layer = 'layer_' + str(self.layer)
-            all_embeddings = glob(os.path.join(self.root_dir, self.model_name, layer, "**/*.npy"), recursive=True)
+            all_features = glob(os.path.join(self.root_dir, self.model_name, layer, "**/*.npy"), recursive=True)
         else:
-            all_embeddings = glob(os.path.join(self.root_dir, self.model_name, "**/*.npy"), recursive=True)
+            all_features = glob(os.path.join(self.root_dir, self.model_name, "**/*.npy"), recursive=True)
 
         if self.num_files == -1: # sample all the data
-            return all_embeddings
+            return all_features
         
-        embeddings_sample = np.random.choice(all_embeddings, self.num_files, replace=False)
-        return embeddings_sample
+        features_sample = np.random.choice(all_features, self.num_files, replace=False)
+        return features_sample
 
-    def load_features(self, files): # Works
+    def load_features(self, files):
         """
-        Load the sampled embeddings from file paths
+        Load the sampled features from file paths
 
         Parameters
         ----------
         self : Class
-            The object containing all information to find alignments for the selected embeddings
+            The object containing all information to find alignments for the selected features
         files : list (String)
-            List of file paths to the sampled embeddings
+            List of file paths to the sampled features
 
         Return
         ------
-        embeddings : list
-            A list of embeddings loaded from the file paths
+        features : list
+            A list of features loaded from the file paths
         """
 
-        embeddings = []
+        features = []
 
-        for file in tqdm(files, desc="Loading Embeddings"):
-            embedding = torch.from_numpy(np.load(file))
-            if len(embedding.shape) == 1: # if only one dimension, add a dimension
-                embeddings.append(embedding.unsqueeze(0))
+        for file in tqdm(files, desc="Loading features"):
+            encodings = torch.from_numpy(np.load(file))
+            if len(encodings.shape) == 1: # if only one dimension, add a dimension
+                features.append(encodings.unsqueeze(0))
             else:
-                embeddings.append(embedding)
-        return embeddings
+                features.append(encodings)
+        return features
     
-    def normalize_features(self, features): # Works
+    def normalize_features(self, features):
         """
-        Normalizes the feature embeddings to have a mean of 0 and a standard deviation of 1
+        Normalizes the features to have a mean of 0 and a standard deviation of 1
 
         Parameters
         ----------
         self : Class
-            The object containing all information to find alignments for the selected embeddings
+            The object containing all information to find alignments for the selected features
         features : numpy.ndarray
-            The feature embeddings to normalize
+            The features to normalize
 
         Returns
         -------
         normalized_features : numpy.ndarray
-            The normalized feature embeddings
+            The normalized features
         """
 
         stacked_features = torch.cat(features, dim=0) # concatenate all features into one tensor with size (sum_seq_len, feature_dim (channels))
@@ -197,89 +122,40 @@ class Features:
             normalized_features.append(torch.from_numpy(scaler.transform(feature))) # (n_samples, n_features)
         return normalized_features
 
-    def get_alignment_paths(self, files): # Works
+    def get_frame_num(self, seconds):
         """
-        Find Paths to the TextGrid files (alignments) corresponding to the sampled embeddings
+        Convert seconds to feature frame number
 
         Parameters
         ----------
         self : Class
-            The object containing all information to find alignments for the selected embeddings
-        files : list (String)
-            List of file paths to the sampled embeddings
-
-        Return
-        ------
-        alignments : list (Path)
-            A list of file paths to the alignment files corresponding to the sampled embeddings
-        """
-
-        alignments = []
-
-        for file in tqdm(files, desc="Sample Alignments"): # TODO make this work better, the -4 is a hack
-            if self.alignment_format == '.TextGrid':
-                sub_dir = file.split("/")[-4:]
-                align_dir = os.path.join(self.data_dir, *sub_dir)
-            elif self.alignment_format == '.txt':
-                sub_dir = file.split("/")[-1]
-                align_dir = os.path.join(self.data_dir, sub_dir)
-            
-            if os.path.exists(Path(align_dir).with_suffix(self.alignment_format)):
-                alignments.append(Path(align_dir).with_suffix(self.alignment_format))
-
-        return alignments
-    
-    def set_alignments(self, files): # Works
-        """
-        Create Alignment_Data objects and set their attributes for each alignment file
-
-        Parameters
-        ----------
-        self : Class
-            The object containing all information to find alignments for the selected embeddings
-        files : list (Path)
-            A list of file paths to the alignment files corresponding to the sampled embeddings
-        """
-            
-        for file in tqdm(files, desc="Reading Alignment"):
-            alignment = Alignment_Data(file, self.alignment_format, type='words')
-            alignment.set_attributes(self)
-            self.alignment_data.append(alignment)
-
-    def get_frame_num(self, seconds): # Works
-        """
-        Convert seconds to feature embedding frame number
-
-        Parameters
-        ----------
-        self : Class
-            The object containing all information to find alignments for the selected embeddings
+            The object containing all information to find alignments for the selected features
         seconds : float
             The number of seconds (of audio) to convert to frames
 
         Return
         ------
         output : int
-            The feature embedding frame number corresponding to the given number of seconds 
+            The feature frame number corresponding to the given number of seconds 
         """
 
         return np.round(seconds / self.frames_per_ms * 1000) # seconds (= samples / sample_rate) / 20ms per frame * 1000ms per second
 
-    def get_sample_second(self, frame_num): # Works
+    def get_sample_second(self, frame_num):
         """
-        Convert feature embedding frame number to seconds
+        Convert feature frame number to seconds
 
         Parameters
         ----------
         self : Class
-            The object containing all information to find alignments for the selected embeddings
+            The object containing all information to find alignments for the selected features
         frame_num : float
-            The frame number (of feature embeddings) to convert to seconds
+            The frame number (of features) to convert to seconds
 
         Return
         ------
         output : double
-            The number of seconds corresponding to the given feature embedding frame number
+            The number of seconds corresponding to the given feature frame number
         """
 
         return frame_num * self.frames_per_ms / 1000 # frame_num * 20ms per frame / 1000ms per second
