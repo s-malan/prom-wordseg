6d152085e6a68d8cfda464d99787c6aa4d009599
s-malan <24227013@sun.ac.za>
2025-06-26 12:45:41 +0200

    O_o

--- extract_seg.py
+++ extract_seg.py
@@ -1,5 +1,5 @@
 """
-Main script to extract features, segment the audio, and evaluate the resulting segmentation.
+Main script to extract unsupervised prominence based word boundaries.
 
 Author: Simon Malan
 Contact: 24227013@sun.ac.za
@@ -13,6 +13,11 @@ import argparse
 import json
 import os
 from pathlib import Path
+import matplotlib.pyplot as plt
+import torch
+import torchaudio
+from glob import glob
+import itertools
 
 def get_features(data, batch_size, batch_num):
     """
@@ -46,14 +51,9 @@ def get_features(data, batch_size, batch_num):
     norm_features = data.normalize_features(features) # normalize the sampled features
 
     index_del = []
-    for i, features in enumerate(norm_features): # delete features with only one frame
-        if features.shape[0] == 1:
+    for i, norm_feature in enumerate(norm_features): # delete features with only one frame
+        if norm_feature.shape[0] == 1:
             index_del.append(i)
-
-    # for i in sorted(index_del, reverse=True):
-    #     del sample[i]
-    #     del features[i]
-    #     del norm_features[i]
     
     if len(sample) == 0:
         print('No features to segment, sampled a file with only one frame.')
@@ -113,6 +113,83 @@ def set_alignments(data, sample):
     data.set_alignments(files=alignments) # set the text, start and end attributes of the alignment files
 
 if __name__ == "__main__":
+    def plot_seg(segment, data, peaks, sample):
+        """
+        Plots the distances, moving average, hypothesized peaks and ground truth alignments for the first file in the sample 
+
+        Parameters
+        ----------
+        segment : Segmentor Class
+            Object containing all hyperparameters and methods to segment the features into words
+        data : Features Class
+            Object containing all information to find alignments for the selected features
+        peaks : list (int)
+            The frame indices of the detected peaks
+        """
+
+        # Plot Mel spectrogram:
+        f_s = 16000
+        n_fft = int(0.02*f_s) #20ms window length
+        stride = int(0.0201*f_s) #20ms to result in same number of frames as the speech models
+        transform = torchaudio.transforms.MelSpectrogram(sample_rate=f_s, n_fft=n_fft, hop_length=stride, n_mels=64)
+
+        paper_utt_idx = 206 # LibriSpeech Utterance 3853/163249/3853-163249-0040
+        sample = sample[paper_utt_idx]
+        sample = os.path.split(sample)[-1]
+        file_path = glob(os.path.join("/media/hdd/data/librispeech/dev-clean", "**" , Path(sample).with_suffix(".flac")), recursive=True)[0]
+        wav, _ = torchaudio.load(file_path, backend='soundfile')
+        mel_specgram = np.log(transform(wav))
+        # mel_specgram = mel_specgram.permute(0, 2, 1)
+        if torch.isinf(mel_specgram).any():
+            mel_specgram[torch.isinf(mel_specgram)] = torch.nan
+
+        fig, ax = plt.subplots(figsize=(9, 5))
+        ax.imshow(mel_specgram.squeeze(), origin="lower", interpolation="nearest", zorder=0)
+
+        # Plot the distances, moving average and peaks
+        peaks = peaks[paper_utt_idx]
+        distances = segment.distances[paper_utt_idx]*13 + (13)
+        smoothed_distances = segment.smoothed_distances[paper_utt_idx]*13 + (13)
+        
+        # _, ax = plt.subplots()
+        ax.plot(distances, label='Distances', color='red', alpha=0.7, linewidth=1.8, zorder=1)
+        # ax.plot(np.range(segment.distances[paper_utt_idx]), segment.distances[paper_utt_idx] label='Distances', color='blue')
+        ax.plot(smoothed_distances, label='Smooth Distances', color='white', alpha=1.0, linewidth=2.45, zorder=2)
+        ax.scatter(peaks, smoothed_distances[peaks], marker='+', label='Peaks', color='black', s=200, linewidth=2.45, zorder=3)
+        ax.scatter(peaks, smoothed_distances[peaks], marker='+', label='Peaks', color='white', s=150, linewidth=2.15, zorder=4)
+
+        ax.fill_between([-5, len(smoothed_distances)], [-0.5,-200], color='white')
+
+        alignment_end_frames = data.alignment_data[paper_utt_idx].end
+        alignment_end_times = [data.get_sample_second(end_frame) for end_frame in alignment_end_frames]
+        segments = [(a + ((b-a)//2)) for a, b in itertools.pairwise(np.concatenate(([0],alignment_end_frames)))]
+        print('Segment end times, frames, and text')
+        print(alignment_end_times)
+        print(alignment_end_frames)
+        print(segments)
+        print(data.alignment_data[paper_utt_idx].text)
+
+        ax.set_ylim(ymin=-10)
+        for frame in alignment_end_frames:
+            ax.axvline(x=frame, label='Ground Truth', color='black', linewidth=0.8)
+            # ax.axvline(x=frame-data.get_frame_num(0.02), color='black', linewidth=0.2, alpha=0.5) # 0.02s tolerance
+            # ax.axvline(x=frame+data.get_frame_num(0.02), color='black', linewidth=0.2, alpha=0.5)
+        
+        custom_tick_labels = data.alignment_data[paper_utt_idx].text
+        ax.set_xticks([])
+        ax.set_yticks([])
+        font_size = 15/alignment_end_frames[-1] * 140
+        y_lim = 150/alignment_end_frames[-1] * - 6
+        # y_lim = (font_size * -0.8) - (font_size*0.025)
+        for i, midpoint in enumerate(segments):
+            ax.text(midpoint, -1, custom_tick_labels[i], ha='center', va='top', fontsize=font_size)
+
+        ax.axhline(y=0, color='black', linewidth=0.8)
+        ax.set_xlim(xmin=alignment_end_frames[0]-5)
+        ax.set_xlim(xmax=alignment_end_frames[-2]+5)
+        ax.set_ylim(ymin=y_lim, ymax=63)
+        plt.tight_layout()
+        plt.savefig('distances.pdf', bbox_inches='tight')
   
     parser = argparse.ArgumentParser(description="Segment speech audio.")
     parser.add_argument(
@@ -150,7 +227,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '--batch_size',
         default=25000,
-        int=Path,
+        type=int,
     )
 
     args = parser.parse_args()
@@ -168,6 +245,9 @@ if __name__ == "__main__":
                 params = params[args.model][str(args.layer)]
             else:
                 params = params[args.model]
+            dist = params['distance']
+            window = params['window_size']
+            prom = params['prominence']
     
     if args.model in ["mfcc", "melspec"]:
         frames_per_ms = 10
@@ -193,20 +273,17 @@ if __name__ == "__main__":
         # Segmenting
         peaks, prominences, segmentor = get_word_segments(norm_features, distance_type=dist, prominence=prom, window_size=window)
 
+        # Ensure last frame boundaries (except if last boundary is within tolerance of last frame)
+        for i, peak in enumerate(peaks):
+            if len(peak) == 0:
+                peak = np.array([features[i].shape[0] - 1]) # add a peak at the end of the file
+            elif peak[-1] != features[i].shape[0] and (peak[-1] != features[i].shape[0] - 1 or (frames_per_ms == 10 and peak[-1] != features[i].shape[0] - 2)): # add at last frame (if not there or in tolerance)
+                peak = np.append(peak, features[i].shape[0] - 1)
+            peaks[i] = peak
+
         # Add samples and peaks for features with only one frame
         sample.extend(sample_one_frame)
         peaks.extend([np.array([1]) for _ in range(len(sample_one_frame))])
-
-        # Ensure last frame boundaries (except if last boundary is within tolerance of last frame)
-        for i, peak in enumerate(peaks):
-            if len(peak) == 0: # -1 to compensate for padding
-                peak = np.array([features[i].shape[0] - 1]) # add a peak at the end of the file
-            elif i < len(features): # ensure there is a peak at the last frame (at samples longer than one frame)
-                # if abs(peak[-1] - features[i].shape[0]) < 2: # in tolerance: remove and add at last frame
-                #     peak[-1] = features[i].shape[0] - 1
-                if peak[-1] != features[i].shape[0] and peak[-1] != features[i].shape[0] - 1: # not in tolerance: add at last frame
-                    peak = np.append(peak, features[i].shape[0] - 1)
-            peaks[i] = peak
 
         # Optionally save the output segment boundaries
         if args.save_out is not None:
--- wordseg/segment.py
+++ wordseg/segment.py
@@ -7,13 +7,11 @@ Date: March 2024
 """
 
 import numpy as np
-import argparse
-from pathlib import Path
 from tqdm import tqdm
 from scipy.spatial import distance
 from scipy.signal import find_peaks
-from scipy.signal import peak_prominences # used to find the prominences of the peaks
-from sklearn.preprocessing import StandardScaler # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
+from scipy.signal import peak_prominences
+from sklearn.preprocessing import StandardScaler
 
 class Segmentor:
     """
@@ -42,7 +40,7 @@ class Segmentor:
         self.distances = []
         self.smoothed_distances = []
         
-    def get_distance(self, embeddings): # Works
+    def get_distance(self, embeddings):
         """
         Calculates the normalized distance between each embedding in the sequence
 
@@ -71,7 +69,7 @@ class Segmentor:
             else:
                 raise ValueError("Distance type not supported")
 
-    def moving_average(self): # Works
+    def moving_average(self):
         """
         Calculates the moving average of the distances
 
@@ -82,11 +80,11 @@ class Segmentor:
         """
 
         for dist in tqdm(self.distances, desc="Moving Average"):
-            dist = np.pad(dist, (self.window_size // 2, self.window_size // 2), mode='edge') # TODO check if this is the correct padding
+            dist = np.pad(dist, (self.window_size // 2, self.window_size // 2), mode='edge')
             box = np.ones(self.window_size) / self.window_size
             self.smoothed_distances.append(np.convolve(dist, box, 'valid'))
 
-    def peak_detection(self): # Works
+    def peak_detection(self):
         """
         Finds the peaks in the distances between feature embeddings
 
@@ -113,96 +111,3 @@ class Segmentor:
             prominences.append(prominences_found)
         
         return peaks, prominences
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Encode an audio dataset.")
-    parser.add_argument(
-        "model",
-        help="available models (wav2vec2.0: HuggingFace, fairseq, hubert: HuggingFace, fairseq, hubert:main)",
-        choices=["w2v2_hf", "w2v2_fs", "hubert_hf", "hubert_fs", "hubert_shall"],
-        default="w2v2_hf",
-    )
-    parser.add_argument(
-        "layer",
-        help="available models (wav2vec2.0: HuggingFace, fairseq, hubert: HuggingFace, fairseq, hubert:main)",
-        type=int,
-    )
-    parser.add_argument(
-        "embeddings_dir",
-        metavar="embeddings-dir",
-        help="path to the embeddings directory.",
-        type=Path,
-    )
-    parser.add_argument(
-        "alignments_dir",
-        metavar="alignments-dir",
-        help="path to the alignments directory.",
-        type=Path,
-    )
-    parser.add_argument(
-        "sample_size",
-        metavar="sample-size",
-        help="number of embeddings to sample.",
-        type=int,
-    )
-    parser.add_argument(
-        "--align_format",
-        help="extension of the alignment files (defaults to .TextGrid).",
-        default=".TextGrid",
-        type=str,
-    )
-
-    args = parser.parse_args() #python3 wordseg/segment.py w2v2_hf 12 /media/hdd/embeddings /media/hdd/data/librispeech_alignments 5
-    
-    # data = utils.Features(root_dir=args.embeddings_dir, model_name=args.model, layer=args.layer, data_dir=args.alignments_dir, alignment_format=args.align_format, num_files=args.sample_size)
-
-    # # Embeddings
-    # sample = data.sample_embeddings() # sample from the feature embeddings
-    # # print(sample)
-    # embeddings = data.load_embeddings(sample) # load the sampled embeddings
-    # norm_embeddings = data.normalize_features(embeddings) # normalize the sampled embeddings
-    # print('original embedding shape', norm_embeddings[0].shape)
-    
-    # # Segmenting
-    # segment = Segmentor(distance_type="cosine", prominence=0.65, window_size=2) # window_size is in frames (1 frame = 20ms for wav2vec2 and HuBERT)
-    # segment.get_distance(norm_embeddings)
-    # print('distance shape', segment.distances[0].shape)
-
-    # segment.moving_average() # calculate the moving average of the distances
-    # print('smoothed distance shape', segment.smoothed_distances[0].shape)
-
-    # print('mean distance (normalized)', np.mean(segment.smoothed_distances[0]))
-    # print('std distance (normalized)', np.std(segment.smoothed_distances[0]))
-
-    # peaks, prominences = segment.peak_detection() # find the peaks in the distances
-    # print('peaks, prominences', peaks[0], prominences[0])
-
-    # # Alignments
-    # alignments = data.get_alignment_paths(files=sample) # get the paths to the alignments corresponding to the sampled embeddings
-    # # print(alignments)
-    # data.set_alignments(files=alignments) # set the text, start and end attributes of the alignment files
-    # # print(data.alignment_data[0])
-
-    # # Plot the distances, moving average and peaks AND compare to the alignments
-    # fig, ax = plt.subplots()
-    # ax.plot(segment.distances[0], label='Distances', color='blue', alpha=0.5)
-    # # ax.plot(np.range(segment.distances[0]), segment.distances[0] label='Distances', color='blue')
-    # ax.plot(segment.smoothed_distances[0], label='Smooth Distances', color='red', alpha=0.5)
-    # ax.scatter(peaks, segment.smoothed_distances[0][peaks], marker='x', label='Peaks', color='green')
-    
-    # alignment_end_times = data.alignment_data[0].end
-    # alignment_end_frames = [data.get_frame_num(end_time) for end_time in alignment_end_times]
-    # print('Alignment end times and frames:')
-    # print(alignment_end_times)
-    # print(alignment_end_frames)
-    # print(data.alignment_data[0].text)
-
-    # for frame in alignment_end_frames:
-    #     ax.axvline(x=frame, label='Ground Truth', color='black', linewidth=0.5)
-    
-    # custom_ticks = alignment_end_frames
-    # custom_tick_labels = data.alignment_data[0].text
-    # ax.set_xticks(custom_ticks)
-    # ax.set_xticklabels(custom_tick_labels, rotation=90, fontsize=6)
-
-    # plt.savefig('distances.png', dpi=300)    
